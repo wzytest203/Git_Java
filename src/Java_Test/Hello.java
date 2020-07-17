cc2f9fefaf9af82840dafb71d32ddb1ab4b0412e
admin <wangzhaoyang@piesat.cn>
2020-07-17 13:37:09 +0800

    test

---
+++ src/Java_Test/Hello.java
@@ -0,0 +1,14 @@
+package Java_Test;
+public class Hello {
+    public static void main(String []args) {
+        System.out.println("Hello World");
+        System.out.println("Hello Selenium");
+        System.out.println("Hello HTHT");
+        System.out.println("Hello dajia1");
+        System.out.println("Hello dajia2");
+        System.out.println("Hello dajia3");
+        System.out.println("Hello dajia4");
+        System.out.println("Hello dajia5");
+    }
+}
+
